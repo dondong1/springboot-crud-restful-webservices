b0e661ce223f51362efc2952a338d6f69dc946ef
dondong1 <don_nguyen2001@yahoo.com>
2021-12-24 20:03:10 -0800

    first

---
+++ src/main/java/com/don/SpringstressfulApplication.java
@@ -0,0 +1,13 @@
+package com.don;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstressfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstressfulApplication.class, args);
+	}
+
+}
